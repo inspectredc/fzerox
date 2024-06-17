b6fa99bec54cfac8792a7d964ab5f593e11c252c
inspectredc <inspectredc@gmail.com>
2024-06-17 14:10:22 +0100

    first few c files building

---
+++ src/game/1060.c
@@ -0,0 +1,21 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/rev0/nonmatchings/game/1060/func_80067060.s")
+
+#pragma GLOBAL_ASM("asm/us/rev0/nonmatchings/game/1060/func_800670E8.s")
+
+#pragma GLOBAL_ASM("asm/us/rev0/nonmatchings/game/1060/func_80067118.s")
+
+#pragma GLOBAL_ASM("asm/us/rev0/nonmatchings/game/1060/func_8006715C.s")
+
+#pragma GLOBAL_ASM("asm/us/rev0/nonmatchings/game/1060/func_800671A8.s")
+
+#pragma GLOBAL_ASM("asm/us/rev0/nonmatchings/game/1060/D_800D30D0.s")
+
+#pragma GLOBAL_ASM("asm/us/rev0/nonmatchings/game/1060/D_800D30EC.s")
+
+#pragma GLOBAL_ASM("asm/us/rev0/nonmatchings/game/1060/func_800671EC.s")
+
+#pragma GLOBAL_ASM("asm/us/rev0/nonmatchings/game/1060/func_800678B8.s")
+
+#pragma GLOBAL_ASM("asm/us/rev0/nonmatchings/game/1060/func_80067A10.s")
---
+++ src/game/F0B0.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/rev0/nonmatchings/game/F0B0/func_800750B0.s")
+
+#pragma GLOBAL_ASM("asm/us/rev0/nonmatchings/game/F0B0/func_8007515C.s")
