959634e0a54f0858d5542dfbf2c6d4920022f19e
inspectredc <inspectredc@gmail.com>
2025-01-22 12:06:39 +0000

    use correct function name for non_matching

--- src/game/36ED0.c
+++ src/game/36ED0.c
@@ -1090,13 +1090,13 @@ void func_800A0068(unk_36ED0* arg0, f32 arg1, f32 arg2, Mtx3F* arg3, Vec3f* arg4
     temp_fs5 = arg2 + 11.5f;
     temp_fs1 = (temp_fs4 - temp_fs5) / 2.0f;
 
-    arg0->unk_26[0] = func_8006A9E0(arg8->x = arg0->unk_14.x + (temp_fs1 * arg3->xz));
-    arg0->unk_26[1] = func_8006A9E0(arg8->y = arg0->unk_14.y + (temp_fs1 * arg3->yz));
-    arg0->unk_26[2] = func_8006A9E0(arg8->z = arg0->unk_14.z + (temp_fs1 * arg3->zz));
+    arg0->unk_26[0] = Math_Round(arg8->x = arg0->unk_14.x + (temp_fs1 * arg3->xz));
+    arg0->unk_26[1] = Math_Round(arg8->y = arg0->unk_14.y + (temp_fs1 * arg3->yz));
+    arg0->unk_26[2] = Math_Round(arg8->z = arg0->unk_14.z + (temp_fs1 * arg3->zz));
 
-    arg0->unk_20[0] = func_8006A9E0(arg8->x + ((argB - argC - argD) * arg3->xy));
-    arg0->unk_20[1] = func_8006A9E0(arg8->y + ((argB - argC - argD) * arg3->yy));
-    arg0->unk_20[2] = func_8006A9E0(arg8->z + ((argB - argC - argD) * arg3->zy));
+    arg0->unk_20[0] = Math_Round(arg8->x + ((argB - argC - argD) * arg3->xy));
+    arg0->unk_20[1] = Math_Round(arg8->y + ((argB - argC - argD) * arg3->yy));
+    arg0->unk_20[2] = Math_Round(arg8->z + ((argB - argC - argD) * arg3->zy));
 
     temp_fs1 = (temp_fs4 + temp_fs5) / 2.0f;
 
@@ -1108,35 +1108,35 @@ void func_800A0068(unk_36ED0* arg0, f32 arg1, f32 arg2, Mtx3F* arg3, Vec3f* arg4
     arg7->y = arg8->y - temp_fs3;
     arg7->z = arg8->z - temp_fs0;
 
-    arg0->unk_38[0] = func_8006A9E0(arg4->x = (temp_fs4 = arg3->xz * temp_fs1) + arg8->x);
-    arg0->unk_38[1] = func_8006A9E0(arg4->y = (temp_fs5 = arg3->yz * temp_fs1) + arg8->y);
-    arg0->unk_38[2] = func_8006A9E0(arg4->z = (sp6C = arg3->zz * temp_fs1) + arg8->z);
+    arg0->unk_38[0] = Math_Round(arg4->x = (temp_fs4 = arg3->xz * temp_fs1) + arg8->x);
+    arg0->unk_38[1] = Math_Round(arg4->y = (temp_fs5 = arg3->yz * temp_fs1) + arg8->y);
+    arg0->unk_38[2] = Math_Round(arg4->z = (sp6C = arg3->zz * temp_fs1) + arg8->z);
 
-    arg0->unk_4A[0] = func_8006A9E0(arg5->x = arg8->x - temp_fs4);
-    arg0->unk_4A[1] = func_8006A9E0(arg5->y = arg8->y - temp_fs5);
-    arg0->unk_4A[2] = func_8006A9E0(arg5->z = arg8->z - sp6C);
+    arg0->unk_4A[0] = Math_Round(arg5->x = arg8->x - temp_fs4);
+    arg0->unk_4A[1] = Math_Round(arg5->y = arg8->y - temp_fs5);
+    arg0->unk_4A[2] = Math_Round(arg5->z = arg8->z - sp6C);
 
     temp_fs1 += arg9;
 
-    arg0->unk_32[0] = func_8006A9E0((temp_fs2 = arg8->x + (argB * arg3->xy)) + (temp_fs4 = arg3->xz * temp_fs1));
-    arg0->unk_32[1] = func_8006A9E0((temp_fs3 = arg8->y + (argB * arg3->yy)) + (temp_fs5 = arg3->yz * temp_fs1));
-    arg0->unk_32[2] = func_8006A9E0((temp_fs0 = arg8->z + (argB * arg3->zy)) + (sp6C = arg3->zz * temp_fs1));
+    arg0->unk_32[0] = Math_Round((temp_fs2 = arg8->x + (argB * arg3->xy)) + (temp_fs4 = arg3->xz * temp_fs1));
+    arg0->unk_32[1] = Math_Round((temp_fs3 = arg8->y + (argB * arg3->yy)) + (temp_fs5 = arg3->yz * temp_fs1));
+    arg0->unk_32[2] = Math_Round((temp_fs0 = arg8->z + (argB * arg3->zy)) + (sp6C = arg3->zz * temp_fs1));
 
-    arg0->unk_44[0] = func_8006A9E0(temp_fs2 - temp_fs4);
-    arg0->unk_44[1] = func_8006A9E0(temp_fs3 - temp_fs5);
-    arg0->unk_44[2] = func_8006A9E0(temp_fs0 - sp6C);
+    arg0->unk_44[0] = Math_Round(temp_fs2 - temp_fs4);
+    arg0->unk_44[1] = Math_Round(temp_fs3 - temp_fs5);
+    arg0->unk_44[2] = Math_Round(temp_fs0 - sp6C);
 
     temp_fs1 += argA;
 
     arg0->unk_2C[0] =
-        func_8006A9E0((temp_fs2 = arg8->x + ((argB - argC) * arg3->xy)) + (temp_fs4 = arg3->xz * temp_fs1));
+        Math_Round((temp_fs2 = arg8->x + ((argB - argC) * arg3->xy)) + (temp_fs4 = arg3->xz * temp_fs1));
     arg0->unk_2C[1] =
-        func_8006A9E0((temp_fs3 = arg8->y + ((argB - argC) * arg3->yy)) + (temp_fs5 = arg3->yz * temp_fs1));
-    arg0->unk_2C[2] = func_8006A9E0((temp_fs0 = arg8->z + ((argB - argC) * arg3->zy)) + (sp6C = arg3->zz * temp_fs1));
+        Math_Round((temp_fs3 = arg8->y + ((argB - argC) * arg3->yy)) + (temp_fs5 = arg3->yz * temp_fs1));
+    arg0->unk_2C[2] = Math_Round((temp_fs0 = arg8->z + ((argB - argC) * arg3->zy)) + (sp6C = arg3->zz * temp_fs1));
 
-    arg0->unk_3E[0] = func_8006A9E0(temp_fs2 - temp_fs4);
-    arg0->unk_3E[1] = func_8006A9E0(temp_fs3 - temp_fs5);
-    arg0->unk_3E[2] = func_8006A9E0(temp_fs0 - sp6C);
+    arg0->unk_3E[0] = Math_Round(temp_fs2 - temp_fs4);
+    arg0->unk_3E[1] = Math_Round(temp_fs3 - temp_fs5);
+    arg0->unk_3E[2] = Math_Round(temp_fs0 - sp6C);
 }
 #else
 void func_800A0068(unk_36ED0* arg0, f32 arg1, f32 arg2, Mtx3F* arg3, Vec3f* arg4, Vec3f* arg5, Vec3f* arg6, Vec3f* arg7,
